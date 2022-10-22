8f32965d719b73a2c25df13b0280bd8d10f8b379
yinxiang <790038148@qq.com>
2022-10-22 12:25:13 +0800

    Init without orb.txt

---
+++ build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yinxiang/slam/orb_slam2/src/Converter.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/Frame.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/FrameDrawer.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/Initializer.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/KeyFrame.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/KeyFrameDatabase.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/LocalMapping.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/LoopClosing.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/Map.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/MapDrawer.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/MapPoint.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/ORBextractor.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/ORBmatcher.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/Optimizer.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/PnPsolver.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/Sim3Solver.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/System.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/Tracking.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/yinxiang/slam/orb_slam2/src/Viewer.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"x11\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/usr/include/opencv"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_core/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_display/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_opengl/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_image/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_windowing/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/build/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_vars/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_geometry/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/tinyobj/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_glgeometry/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_packetstream/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_plot/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_scene/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_tools/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/rgbd_tum.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yinxiang/slam/orb_slam2/Examples/RGB-D/rgbd_tum.cc" "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/rgbd_tum.dir/Examples/RGB-D/rgbd_tum.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"x11\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/usr/include/opencv"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_core/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_display/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_opengl/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_image/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_windowing/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/build/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_vars/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_geometry/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/tinyobj/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_glgeometry/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_packetstream/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_plot/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_scene/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_tools/include"
+  "/home/yinxiang/slam/Pangolin/Pangolin/components/pango_video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yinxiang/slam/orb_slam2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
